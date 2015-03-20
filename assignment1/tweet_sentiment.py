805f6be2a692e568acd714059a58a9bcde0ec737
hanhan-pku <hanhan198312@gmail.com>
2015-03-20 00:21:00 -0400

    problem 2 check in

--- assignment1/tweet_sentiment.py
+++ assignment1/tweet_sentiment.py
@@ -1,7 +1,21 @@
 import sys
+import json
 
-def hw():
-    print 'Hello, world!'
+def prob2():
+    afinnfile = open("AFINN-111.txt")
+    scores = {}
+    for line in afinnfile:
+        term, score = line.split("\t")
+        scores[term] = int(score) 
+    tweet_file = open("output.txt") 
+    for line in tweet_file:
+        pline = json.loads(line)
+        sentscore = 0
+        if 'text' in pline: 
+            for word in pline['text'].split(" "):
+                if word in scores.keys():
+                    sentscore += scores[word]
+        print sentscore 
 
 def lines(fp):
     print str(len(fp.readlines()))
@@ -9,9 +23,9 @@ def lines(fp):
 def main():
     sent_file = open(sys.argv[1])
     tweet_file = open(sys.argv[2])
-    hw()
-    lines(sent_file)
-    lines(tweet_file)
+    prob2()
+    #lines(sent_file)
+    #lines(tweet_file)
 
 if __name__ == '__main__':
     main()
